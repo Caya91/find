55e798523686cfdea23d8a7915674c9671a9cac9
martinc <marti.carnein@hotmail.de>
2023-10-13 22:10:58 +0200

    forgotten changes xd

--- find.cc
+++ find.cc
@@ -3,23 +3,40 @@
 
 int main(int argc, char *argv[]) {
     //initialise argument parser
-    argparse::ArgumentParser program("program_name");
+    argparse::ArgumentParser find_args("find");
     //adding arguments to the parser
-    program.add_argument("-type")
+    find_args.add_argument("directory").default_value(".").required()
+        .help("Verzeichnis in dem gesucht werden soll");
+    find_args.add_argument("-type")
         .help("f fuer file | d fuer directory");
-    program.add_argument("-name")
+    find_args.add_argument("-name")
         .default_value("*")
         .help("Akzeptiert wildcards");
-    program.add_argument("-xdev")
-        .default_value(false)
+    find_args.add_argument("-xdev")
+        .default_value(false).implicit_value(true)
         .help("Nur auf dem aktuellen Dateisystem suchen");
-    program.add_argument("-follow")
-        .default_value(false)
+    find_args.add_argument("-follow")
+        .default_value(false).implicit_value(true)
         .help("Symbolic links folgen");
-// hier noch .remaining einfügen um überschüssige Argumente zu speichern
+    // hier noch .remaining einfügen um überschüssige Argumente zu speichern
 
+    // argumente parsen
 
-    std::cout << program;
+    try{
+
+        find_args.parse_args(argc, argv);
+    } catch (const std::runtime_error& err){
+        std::cerr << err.what() << std::endl;
+        std::cerr << find_args;
+        exit(0);
+    }
+    std::cout << find_args.get("directory") << std::endl;
+    std::cout << find_args << std::endl;
+
+    auto a = find_args.get<bool>("-xdev");
+    std::cout << a << std::endl;
+    auto b = find_args.get<bool>("-follow");
+    std::cout << b << std::endl;
 
     return 0;
 }
